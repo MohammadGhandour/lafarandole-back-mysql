3f0bbed552043eb151c8d9130bab6bfa7390f7c3
MohammadGhandour <mohammadghandour02@outlook.com>
2022-10-15 13:49:21 +0300

    Addid routes

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,12 +5,12 @@ const multer = require("../middlewares/multer");
 const validateProductRequest = require("../middlewares/validateProductRequest");
 const router = express.Router();
 
-router.post('/', auth, multer, validateProductRequest, productsCtrl.addProduct);
-router.get('/', auth, productsCtrl.getAllProducts);
-router.get('/byId/:id', auth, productsCtrl.getOneProduct);
-router.get('/products-sold', auth, productsCtrl.getProductsSold);
-router.get('/product-by-barcode/:barcode', auth, productsCtrl.getProductByBarcode);
-router.put('/:id', auth, multer, validateProductRequest, productsCtrl.updateProduct);
-router.delete('/:id', auth, productsCtrl.deleteProduct);
+router.post('/', auth, multer, validateProductRequest, productsCtrl.addProduct)
+router.get('/', auth, productsCtrl.getAllProducts)
+router.get('/byId/:id', auth, productsCtrl.getOneProduct)
+router.get('/products-sold', auth, productsCtrl.getProductsSold)
+router.get('/product-by-barcode/:barcode', auth, productsCtrl.getProductByBarcode)
+router.put('/:id', auth, multer, validateProductRequest, productsCtrl.updateProduct)
+router.delete('/:id', auth, productsCtrl.deleteProduct)
 
 module.exports = router;
