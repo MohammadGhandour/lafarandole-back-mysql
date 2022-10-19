92164f3b8deee01a46749ce933dc14077d9d870e
MohammadGhandour <mohammadghandour02@outlook.com>
2022-10-19 16:26:11 +0300

    Added the backup 19/10/2022/4:26pm

---
+++ database-backups/systemcustomers.sql
@@ -0,0 +1,27 @@
+/*
+-- Query: SELECT * FROM lafarandolesystemdatabase.Customers
+LIMIT 0, 1000
+
+-- Date: 2022-10-19 12:36
+*/
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (1,'Ola Khalife','71387566',1,11.99,NULL,'2022-10-06 18:29:27','2022-10-06 18:29:27');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (2,'Lara Fawwaz','70436952',1,50.15,NULL,'2022-10-07 16:23:19','2022-10-15 11:04:58');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (3,'Bassima Ghandour','03111218',1,63.15,NULL,'2022-10-07 17:46:05','2022-10-15 11:06:30');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (4,'Bothaina El Halabi','76787955',1,79.98,NULL,'2022-10-07 18:00:16','2022-10-07 18:00:16');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (5,'Fatima El Hajj','71434153',1,43.99,NULL,'2022-10-07 18:15:47','2022-10-07 18:15:47');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (6,'Dalia Fakhoury','03029404',1,49.97,NULL,'2022-10-08 11:37:35','2022-10-08 11:37:35');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (7,'Roudaina El Amine','70838029',1,75.96,NULL,'2022-10-09 11:14:03','2022-10-09 11:14:03');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (8,'Ghiwa Farhat','71557051',1,213.19,NULL,'2022-10-09 15:34:11','2022-10-09 15:34:11');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (9,'Nadia Jouni','03133273',2,14.38,NULL,'2022-10-11 09:56:31','2022-10-11 09:59:06');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (10,'Noura Sabaya','70751782',1,14.99,NULL,'2022-10-11 14:55:55','2022-10-11 14:55:55');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (11,'Alaa Ghandour','81159853',1,0.00,NULL,'2022-10-12 09:53:01','2022-10-12 09:53:01');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (12,'Hassan Zaher','70842221',1,34.99,NULL,'2022-10-12 12:34:21','2022-10-12 12:34:21');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (13,'Ali Ghandour','71721963',1,14.99,NULL,'2022-10-14 10:03:07','2022-10-14 10:03:07');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (14,'Malak Ghandour','70915973',1,9.99,NULL,'2022-10-14 10:28:27','2022-10-14 10:28:27');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (15,'Jala Wehbe','03500835',1,61.98,NULL,'2022-10-14 13:40:00','2022-10-14 13:40:00');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (16,'Lara Hamieh','03861989',1,49.99,NULL,'2022-10-15 07:44:30','2022-10-15 07:44:30');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (17,'Inass El Sayyid','71592068',1,54.98,NULL,'2022-10-15 07:45:51','2022-10-15 07:45:51');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (18,'Rayane Ghandour','70509010',1,21.57,NULL,'2022-10-15 10:18:58','2022-10-15 10:18:58');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (19,'Myrna Zaidan','71370083',1,121.45,NULL,'2022-10-16 09:24:38','2022-10-16 09:24:38');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (20,'Sara Issa','76842913',1,46.98,NULL,'2022-10-16 14:28:32','2022-10-16 14:28:32');
+INSERT INTO `Customers` (`id`,`customerName`,`customerNumber`,`numberOfOrders`,`totalOfAllOrders`,`testNewProperty`,`createdAt`,`updatedAt`) VALUES (21,'Nariman Mourad','11111111',1,12.99,NULL,'2022-10-18 13:49:11','2022-10-18 13:49:11');
