91bd5a0ea77af52fb417491e7b4aef4440136645
MohammadGhandour <mohammadghandour02@outlook.com>
2022-10-27 14:15:05 +0300

    Handled the auth error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,15 +3,19 @@ require('dotenv').config();
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'PDQ8v94SswOcgMnqlUTd');
-        const userId = decodedToken.userId;
-        req.auth = { userId };
+        if (req.headers.authorization) {
+            const token = req.headers.authorization.split(' ')[1];
+            const decodedToken = jwt.verify(token, 'PDQ8v94SswOcgMnqlUTd');
+            const userId = decodedToken.userId;
+            req.auth = { userId };
 
-        if (req.body.userId && req.body.userId !== userId) {
-            throw 'User ID non valable !';
+            if (req.body.userId && req.body.userId !== userId) {
+                throw 'User ID non valable !';
+            } else {
+                next();
+            }
         } else {
-            next();
+            res.status(401).json({ error: "You're not authorized 😡" })
         }
     } catch (error) {
         res.status(401).json({ error: error | 'Requête non authentifiée !' });
