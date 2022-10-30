fbc646cc9f9adace2f209b9503f86330f25f1b78
MohammadGhandour <mohammadghandour02@outlook.com>
2022-10-31 01:32:21 +0200

    secured the database connection

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "Moh_mysqlpassword2022",
-    "database": "lafarandolemysql",
-    "host": "localhost",
-    "dialect": "mysql",
-    "port": 3307
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
   },
   "test": {
     "username": "root",
