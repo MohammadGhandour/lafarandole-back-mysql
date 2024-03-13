12e26f7bc4f8fe801b7d58df94f075aa782ed2c5
MohammadGhandour <mohammadghandour02@outlook.com>
2024-03-13 22:53:19 +0200

    ..

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const db = require('./models');
-const cors = require('cors');
+const path = require("path");
+const db = require("./models");
+const cors = require("cors");
 const PORT = process.env.PORT || 2022;
 const FORCE = false;
 const axios = require("axios");
@@ -12,27 +12,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(cors());
-app.use(cors({ origin: 'https://lafarandoleparis.com' }));
+app.use(cors({ origin: ["https://lafarandoleparis.com", "https://invvest.co", "https://backend.invvest.co"] }));
 
-app.use('/api/images', express.static(path.join(__dirname, 'images')));
+app.use("/api/images", express.static(path.join(__dirname, "images")));
 
-app.get('/api', async (req, res) => { res.status(200).json({ message: "Hello from lafarandole backend" }) });
+app.get("/api", async (req, res) => { res.status(200).json({ message: "Hello from lafarandole backend" }) });
 
-const usersRoutes = require('./routes/usersRoutes');
-const productsRoutes = require('./routes/productsRoutes');
-const ordersRoutes = require('./routes/ordersRoutes');
-const customersRoutes = require('./routes/customersRoutes');
-const expensesRoutes = require('./routes/expensesRoutes');
+const usersRoutes = require("./routes/usersRoutes");
+const productsRoutes = require("./routes/productsRoutes");
+const ordersRoutes = require("./routes/ordersRoutes");
+const customersRoutes = require("./routes/customersRoutes");
+const expensesRoutes = require("./routes/expensesRoutes");
 
-app.use('/api/users', usersRoutes);
-app.use('/api/products', productsRoutes);
-app.use('/api/orders', ordersRoutes);
-app.use('/api/customers', customersRoutes);
-app.use('/api/expenses', expensesRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/products", productsRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api/customers", customersRoutes);
+app.use("/api/expenses", expensesRoutes);
 
-// app.use(express.static(path.join(__dirname, '../build')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../build'))
+// app.use(express.static(path.join(__dirname, "../build")));
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../build"))
 // });
 
 app.post("/api/trade-republic/request-sms-code", async (req, res) => {
@@ -180,6 +180,6 @@ db.sequelize.sync({ force: FORCE })
     .then(() => {
         app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); })
     }).catch(err => {
-        console.log('Couldnt connect to database');
+        console.log("Couldnt connect to database");
         console.log(err);
     });
